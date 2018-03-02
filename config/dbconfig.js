bc33b37da61adda026c88044be9e80b33e6d4fd8
Julius Regalado <jreg03@outlook.com>
2018-03-02 11:50:48 -0500

    still trying to deploy to heroku

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,16 +1,15 @@
-
 module.exports = process.env.DATABASE_URL || {
   host:     process.env.DB_HOST || 'localhost',
   port:     process.env.DB_PORT || 5432,
   database: process.env.DB_NAME || 'chefs_diary_db',
+  /* user:     process.env.DB_USER, */
 };
+
 
 /*
-const config = process.env.DATABASE_URL || {
-  host: process.env.PG_HOST,
-  port: process.env.PG_PORT,
-  database: process.env.PG_DATABASE,
-};
-
-module.exports = config;
+module.exports = {
+	host: 'localhost',
+	port: 5432,
+	database: 'chefs_diary_db'	
+}
 */
