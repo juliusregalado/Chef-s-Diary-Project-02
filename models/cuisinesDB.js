1ec0e62d885ff3d750932d659c619204f320f1bf
Julius Regalado <jreg03@outlook.com>
2018-03-05 09:38:54 -0500

    try again

--- models/cuisinesDB.js
+++ models/cuisinesDB.js
@@ -1,5 +1,5 @@
 const pgp = require('pg-promise')();
-const dbConfig = require('./config/dbConfig');
+const dbConfig = require('../../config/dbConfig');
 const db = pgp(dbConfig);
 
 module.exports = {
