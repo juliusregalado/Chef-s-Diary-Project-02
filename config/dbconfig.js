e98124e7c048a81947e69d33a6b5b5b3f9da384f
Julius Regalado <jreg03@outlook.com>
2018-03-05 09:03:38 -0500

    config for heroku

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,16 +1,10 @@
-module.exports = process.env.DATABASE_URL || {
+const config = process.env.DATABASE_URL || {
   host:     process.env.PG_HOST || 'localhost',
   port:     process.env.PG_PORT || 5432,
   database: process.env.PG_DATABASE || 'chefs_diary_db',
   /* user:     process.env.DB_USER, */
 };
 
-/*
-const config = process.env.DATABASE_URL || {
-  host: process.env.PG_HOST,
-  port: process.env.PG_PORT,
-  database: process.env.PG_DATABASE,
-};
+
 
 module.exports = config;
-*/
